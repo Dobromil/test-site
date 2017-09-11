511d234f63a9b09bfa0983953f2419028899a351
Dobromil <dobromilkt@gmail.com>
2017-09-11 23:33:45 +0200

    Add sticky header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,7 +2,11 @@
 import MobileMenu from './modules/mobileMenu'
 import RevealOnScroll from './modules/revealOnScroll'
 import $ from 'jquery';
+import StickyHeader from './modules/stickyHeader'
+
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), "85%");
 new RevealOnScroll($('.testemonial'), "65%");
+
+var stickyHeader = new StickyHeader;
