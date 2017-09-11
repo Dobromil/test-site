863089ab8fca736a9311420c407ac28184b3127a
Dobromil <dobromilkt@gmail.com>
2017-09-11 17:12:53 +0200

    Complete reveal on scroll items

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,4 +1,8 @@
 
 import MobileMenu from './modules/mobileMenu'
+import RevealOnScroll from './modules/revealOnScroll'
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($('.feature-item'), "85%");
+new RevealOnScroll($('.testemonial'), "65%");
